c02533a3a1f9068c34cf438269a61760cc3a5de2
unknown <cogjava4352@iiht.tech>
2022-11-08 18:41:06 +0530

    LocalDateTime

--- java_oops/src/com/java_oops/date/DateTesting.java
+++ java_oops/src/com/java_oops/date/DateTesting.java
@@ -1,6 +1,7 @@
 package com.java_oops.date;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DateTesting {
 
@@ -9,10 +10,12 @@ public class DateTesting {
 		LocalDate date= LocalDate.now();
 		LocalDate yesterday= date.minusDays(1);
 		LocalDate tomorrow = date.plusDays(1);
+		LocalDateTime dateTime = LocalDateTime.now();
 		
 		System.out.println("Current Date is :"+date);
 		System.out.println("Yesterday Date is :"+yesterday);
 		System.out.println("Tomorrow Date is :"+tomorrow);
+		System.out.println("Current Date Time is :"+ dateTime);
 		
 	}
 
