adc26ba84b595c9f01dc0d6dc3469597507b7b81
unknown <cogjava4352@iiht.tech>
2022-11-09 18:03:17 +0530

    HashSet

--- java_oops/src/com/java_oops/collections/HashSetDemo.java
+++ java_oops/src/com/java_oops/collections/HashSetDemo.java
@@ -1,30 +1,32 @@
 package com.java_oops.collections;
 
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 
+public class HashSetDemo {
 
-public class LinkedListDemo {
-	
 	public static void main(String[] args) {
-		
-		LinkedList<String> list = new LinkedList<String>();
-		
+
+		HashSet<String> list = new HashSet<String>();
+
 		list.add("Apple");
 		list.add("Banana");
+		list.add("Banana");
+		list.add("Banana");
+		list.add("Apple");
 		list.add("Mango");
 		list.add("Chiku");
 		list.add("Papaya");
 		System.out.println(list);
 		System.out.println("");
-		
+
 		list.remove(4);
-		list.removeFirst();
-		Iterator itr =list.iterator();
-		while(itr.hasNext()) {
+		
+		Iterator itr = list.iterator();
+		while (itr.hasNext()) {
 			System.out.println(itr.next());
 		}
-		
+
 	}
 
 }
